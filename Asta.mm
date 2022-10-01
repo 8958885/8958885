252db09688a7558e21d7df4f465769c64ddea6e8
8958885 <114800562+8958885@users.noreply.github.com>
2022-10-01 09:36:41 +0200

    Create Asta.mm

---
+++ Asta.mm
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @8958885
+- 👀 I’m interested in asta
+- 🌱 I’m currently learning - skip
+- 💞️ I’m looking to collaborate on - skip
+- 📫 How to reach me - skip
+
+<!---
+8958885/8958885 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
